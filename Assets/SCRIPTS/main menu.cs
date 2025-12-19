981cf0c5865df01e3188cea6ae11768edd351699
b25ee1029 <b25ee1029@iitj.ac.in>
2025-12-19 13:49:23 +0530

    script

---
+++ Assets/SCRIPTS/main menu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmenu : MonoBehaviour
+{
+   public void play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    public void exit()
+    {
+        Application.Quit();
+        Debug.Log("Player Has Quit The Game");
+    }
+}
