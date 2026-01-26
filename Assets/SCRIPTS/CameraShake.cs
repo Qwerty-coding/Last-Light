fcfe30a48b554bdc11977d8e38e064badcb54277
Qwerty-Coded <b25me1036@iitj.ac.in>
2026-01-26 23:11:21 +0530

    Refactor BossZombie for improved attack logic, health management, and UI updates

---
+++ Assets/SCRIPTS/CameraShake.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+    public static CameraShake Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void Shake(float duration, float magnitude)
+    {
+        StartCoroutine(ShakeCoroutine(duration, magnitude));
+    }
+
+    IEnumerator ShakeCoroutine(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(originalPos.x + x, originalPos.y + y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
